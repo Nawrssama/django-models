4523c2e7b9ccc769616bc6b986bccb36d04b500d
Nawrssama <nawrsbibi.nb@gmail.com>
2023-06-14 13:45:23 +0000

    solved the lab

---
+++ snacks/tests.py
@@ -0,0 +1,52 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from .models import Snack
+
+
+class SnacksTests(TestCase):
+    def setUp(self):
+        purchaser = get_user_model().objects.create(username="tester",password="tester")
+        Snack.objects.create(name="rake", purchaser=purchaser)
+
+    def test_list_page_status_code(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_page_template(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_list.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_list_page_context(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        snacks = response.context['snacks']
+        self.assertEqual(len(snacks), 1)
+        self.assertEqual(snacks[0].name, "rake")
+        self.assertEqual(snacks[0].description, 'life is hard lakin jamilaaa')
+        self.assertEqual(snacks[0].purchaser.username, "tester")
+
+    def test_detail_page_status_code(self):
+        url = reverse('snack_detail',args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_page_template(self):
+        url = reverse('snack_detail',args=(1,))
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_detail.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_detail_page_context(self):
+        url = reverse('snack_detail',args=(1,))
+        response = self.client.get(url)
+        snack = response.context['snack']
+        self.assertEqual(snack.name, "rake")
+        self.assertEqual(snack.description, 'life is hard lakin jamilaaa')
+        self.assertEqual(snack.purchaser.username, "tester")
